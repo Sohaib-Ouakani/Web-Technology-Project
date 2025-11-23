7d6da5a2e89096c02cf09a8bac4d08c84a4257ee
Sohaib Ouakani <sohaib.ouakani@studio.unibo.it>
2025-11-23 14:07:50 +0100

    addded login template and the login page assembler

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+//require_once 'bootstrap.php';
+
+//Base Template
+$templateParams["titolo"] = "Volume-Login";
+
+$templateParams["nome"] = "login-template.php";
+
+require 'template/base.php';
+?>
---
+++ template/login-template.php
@@ -0,0 +1,33 @@
+<section class="py-4">
+  <div class="container">
+    <div class="row justify-content-center">
+      
+      <!-- LOGIN CARD -->
+      <div class="col-12 col-md-4">
+        <div class="p-4 rounded" style="background:#e3f1d5;">
+          <h5 class="fw-bold mb-3">Login</h5>
+          
+          <form action="#" method="POST">
+            
+            <div class="mb-3">
+              <label for="username" class="form-label">Username:</label>
+              <input type="text" id="username" name="username" class="form-control" />
+            </div>
+
+            <div class="mb-3">
+              <label for="password" class="form-label">Password:</label>
+              <input type="password" id="password" name="password" class="form-control" />
+            </div>
+
+            <button type="submit" class="btn btn-primary w-100">Invia</button>
+
+          </form>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+
+
